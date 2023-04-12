236c59edd766561ee591aa1452bd269af474bf31
Konrad Szymoniak <szymoniak.konrad@gmail.com>
2023-04-12 23:36:30 +0200

    extract ignore_until_new_line function

--- src/scanner.rs
+++ src/scanner.rs
@@ -2,7 +2,7 @@ use std::{iter::Peekable, str::CharIndices};
 
 use crate::token::{Token, TokenType};
 
-struct ScannerState {
+struct State {
     line: usize,
     start: usize,
     current: usize,
@@ -10,7 +10,7 @@ struct ScannerState {
 
 pub fn scan_tokens(source: &str) -> Vec<Token> {
     let mut tokens = Vec::new();
-    let mut state = ScannerState {
+    let mut state = State {
         line: 1,
         start: 0,
         current: 0,
@@ -62,15 +62,7 @@ pub fn scan_tokens(source: &str) -> Vec<Token> {
             }
             '/' => {
                 if next_matches(&mut chars, '/', &mut state) {
-                    while let Some((_, c)) = chars.peek() {
-                        state.current += 1;
-                        state.start = state.current;
-                        if c == &'\n' {
-                            break;
-                        } else {
-                            chars.next();
-                        }
-                    }
+                    ignore_until_new_line(&mut chars, &mut state);
                 } else {
                     return Some(new_token(TokenType::Slash, source, &mut state));
                 }
@@ -96,29 +88,37 @@ pub fn scan_tokens(source: &str) -> Vec<Token> {
     tokens
 }
 
-fn new_token(token_type: TokenType, source: &str, state: &mut ScannerState) -> Token {
+fn new_token(token_type: TokenType, source: &str, state: &mut State) -> Token {
     let from = state.start;
-    let to = state.current + 1;
-    state.start = to;
-    Token::new(token_type, source[from..to].to_string(), state.line)
+    let to = state.current;
+    state.start = to + 1; // move start position to the next character right after the token
+    Token::new(token_type, source[from..=to].to_string(), state.line)
 }
 
-fn next_matches(
-    chars: &mut Peekable<CharIndices>,
-    next: char,
-    scanner_state: &mut ScannerState,
-) -> bool {
+fn next_matches(chars: &mut Peekable<CharIndices>, next: char, state: &mut State) -> bool {
     match chars.peek() {
         Some((_, c)) => {
             if c == &next {
                 chars.next();
-                scanner_state.current += 1;
+                state.current += 1;
                 true
             } else {
                 false
             }
         }
         None => false,
+    }
+}
+
+fn ignore_until_new_line(chars: &mut Peekable<CharIndices>, state: &mut State) {
+    while let Some((_, c)) = chars.peek() {
+        state.current += 1;
+        state.start = state.current;
+        if c == &'\n' {
+            break;
+        } else {
+            chars.next();
+        }
     }
 }
 
